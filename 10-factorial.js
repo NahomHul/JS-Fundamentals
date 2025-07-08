15b2b1c34f6849063b3af7a918d86fad80912be0
Nahom Hul <nahomhulum@gmail.com>
2025-07-08 22:07:23 +0300

    fix rounding using toExponential

--- 10-factorial.js
+++ 10-factorial.js
@@ -8,10 +8,17 @@ function factorial(number) {
   if (number === 0) return 1;
   let result = 1;
 
-  for (let i = number; i > 0; i-- ) {
-    result = result * i;
+  for (let i = number; i > 0; i--) {
+    result *= i;
   }
-  return result
+  return result;
 }
 
-console.log(factorial(number).toExponential(16));
+const result = factorial(number);
+
+// If result is large, use scientific notation; otherwise, print normally
+if (result >= 1e21) {
+  console.log(result.toExponential(16));
+} else {
+  console.log(result.toString());
+}
