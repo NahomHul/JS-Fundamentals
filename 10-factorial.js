edc727fc8b29e4d159ae218511b06fa780d07445
Nahom Hul <nahomhulum@gmail.com>
2025-07-08 21:59:16 +0300

    fix rounding via BigInt

--- 10-factorial.js
+++ 10-factorial.js
@@ -8,12 +8,12 @@ function factorial(number) {
   if (number === 0n) return 1n;
 
   let result = 1n;
-
   for (let i = number; i > 0n; i--) {
-    result = result * i;
+    result *= i;
   }
-
   return result;
 }
 
-console.log(factorial(number).toString());
+// Convert BigInt result to float for scientific notation
+const result = factorial(number);
+console.log(Number(result).toExponential(16));
